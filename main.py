85c46fbb658f6521fd61e814ba7d5d8ff6540d55
Pieter Moens <pieter@pietermoens.be>
2021-05-14 03:18:29 +0200

    Update configuration

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ from config import ConfigType, ConfigFactory
 
 if __name__ == '__main__':
     # Configuration
-    app_settings = os.getenv('CONFIGURATION ')
-    if app_settings is not None:
-        type_ = ConfigType.reverse_lookup(app_settings)
-    else:
-        type_ = ConfigType.DEVELOPMENT
+    app_settings = os.getenv('CONFIGURATION ', default='deployment')
+    type_ = ConfigType.reverse_lookup(app_settings)
     cfactory = ConfigFactory()
     config = cfactory.get(type_)
 
     # Logging
     logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=config.LOG_LEVEL)
 
+    logging.info(f'Initializing Synthweetix in {app_settings} environment')
     bot = SynthweetixBot(config.TWITTER_CONSUMER_KEY,
                          config.TWITTER_CONSUMER_SECRET,
                          config.TWITTER_ACCESS_TOKEN,
